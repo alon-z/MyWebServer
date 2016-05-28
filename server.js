1d370cb70d25cbddb734f82934de8a8790a630a6
alonikomax <aloniko.zus@gmail.com>
2016-05-29 02:50:33 +0300

    Fix

--- server.js
+++ server.js
@@ -9,25 +9,26 @@ function send404(response) {
 }
 
 function onRequest(request, response) {
-	if(request.method == 'GET' && request.url == '/' ){
+	if(request.method == 'GET' && request.url == '/' ) {
 		response.writeHead(200, {"Content-Type": "text/html"})
 		fs.createReadStream("./index.html").pipe(response);
 	} else {
 		var uri = url.parse(request.url).pathname;
 		filename = path.join(process.cwd(), uri);
 		path.exists(filename, function(exists) {
-		if(!exists) {
-		  fs.readFile('404.html', "binary", function(err, file) {
-		    if(err) {
-		       response.writeHead(404, {"Content-Type": "text/html"});
-		       response.write("404 Not Found\n");
-		    } else {
-		       response.writeHead(404);
-		       response.write(file, "binary");
-		    }
-		    response.end();
-		    return;
-		  }
+			if(!exists) {
+			  fs.readFile('404.html', "binary", function(err, file) {
+			    if(err) {
+			       response.writeHead(404, {"Content-Type": "text/html"});
+			       response.write("404 Not Found\n");
+			    } else {
+			       response.writeHead(404);
+			       response.write(file, "binary");
+			    }
+			    response.end();
+			    return;
+			  }
+			}
 		}
 	}
 }

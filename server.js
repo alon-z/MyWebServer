45f5433586d1c65b4d0f5d9dbe73c63a76024a63
alonikomax <aloniko.zus@gmail.com>
2016-05-29 02:49:36 +0300

    Much better

--- server.js
+++ server.js
@@ -13,10 +13,22 @@ function onRequest(request, response) {
 		response.writeHead(200, {"Content-Type": "text/html"})
 		fs.createReadStream("./index.html").pipe(response);
 	} else {
-		file = fs.createReadStream("." + request.url);
-		file.on('error', send404(response));
-		response.writeHead(200, {"Content-Type": "text/html"})
-		file.pipe(response);
+		var uri = url.parse(request.url).pathname;
+		filename = path.join(process.cwd(), uri);
+		path.exists(filename, function(exists) {
+		if(!exists) {
+		  fs.readFile('404.html', "binary", function(err, file) {
+		    if(err) {
+		       response.writeHead(404, {"Content-Type": "text/html"});
+		       response.write("404 Not Found\n");
+		    } else {
+		       response.writeHead(404);
+		       response.write(file, "binary");
+		    }
+		    response.end();
+		    return;
+		  }
+		}
 	}
 }
 

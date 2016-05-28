796c3668aabc02f8200d4426d14ab984caf5d209
alonikomax <aloniko.zus@gmail.com>
2016-05-29 02:22:43 +0300

    Added GET to all files with 404 exeption

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ function onRequest(request, response) {
 		response.writeHead(200, {"Content-Type": "text/html"})
 		fs.createReadStream("./index.html").pipe(response);
 	} else {
-		send404(response);
+		try {
+			response.writeHead(200, {"Content-Type": "text/html"})
+			fs.createReadStream(request.url).pipe(response);
+		}
+		catch(err) {
+			send404();
+		}
 	}
 }
 
